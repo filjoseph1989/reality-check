51d0e69aa0fc7f34f2f12d14069cfdf270cd4f20
root <root@Desktop-Of-Fil>
2025-03-18 15:05:08 +0800

    adjust indent

--- app/components/FormHeader.tsx
+++ app/components/FormHeader.tsx
@@ -4,30 +4,30 @@ import juiceBox from '../../public/juicebox.svg';
 import arrow from '../../public/arrow.svg';
 
 interface FormHeaderProps {
-    onBack: () => void;
-    onRefresh: () => void;
+  onBack: () => void;
+  onRefresh: () => void;
 }
 
 export default function FormHeader({ onBack, onRefresh }: FormHeaderProps) {
-    return (
-        <header>
-            <nav>
-                <div>
-                    <div>
-                        <div className="back-button-container" onClick={onBack}>
-                            <Image src={arrow} alt="Back Icon" />
-                        </div>
-                    </div>
-                    <div>
-                        <Image src={juiceBox} alt="Juicebox Icon" />
-                    </div>
-                    <div>
-                        <div>
-                            <Image src={refreshIcon} alt="Refresh Icon" onClick={onRefresh} />
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav>
+        <div>
+          <div>
+            <div className="back-button-container" onClick={onBack}>
+              <Image src={arrow} alt="Back Icon" />
+            </div>
+          </div>
+          <div>
+            <Image src={juiceBox} alt="Juicebox Icon" />
+          </div>
+          <div>
+            <div>
+              <Image src={refreshIcon} alt="Refresh Icon" onClick={onRefresh} />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
